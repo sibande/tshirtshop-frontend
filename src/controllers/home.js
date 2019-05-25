501cdf5b92c9738b624de18c3b7924b59b7f6561
Jose Sibande <me@sibande.com>
2019-05-25 04:46:55 +0200

    Style pagination and add items per view row
    Also updates product quick view to not completely rely on javascript

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -7,7 +7,7 @@ import AttributeService from './../services/attribute';
 import {getParameterByName} from './../shared/urls';
 import {handleAddToCartEvent} from './shoppingcart';
 
-import {ITEMS_PER_PAGE} from './../constants';
+import {ITEMS_PER_PAGE_LIST} from './../constants';
 
 
 var productService = new ProductService();
@@ -32,91 +32,51 @@ function renderShoppingcartSummary() {
   }
 }
 
-
-function handleAddToCarMouseLeave(e) {
-  var currentElem = e.target;
-
-  currentElem.remove();
-}
-
-
 function handleAddToCartMouseOver(e) {
-  var latestHoverProduct = null;
-
-  var loadingQuickView = '<div class="quick-add-to-cart">'
-      + '<img src="/static/img/ajax_small.gif" class="quick-view-loading" alt="Loading..."></div>';
-
   var elem = e.target;
-  if (elem.classList.contains('product-item') || elem.closest('div.product-item')) {
 
+  if (elem.classList.contains('product-item-wrapper') || elem.closest('div.product-item-wrapper')) {
     var currentElem;
-    if (elem.classList.contains('product-item')) {
+    if (elem.classList.contains('product-item-wrapper')) {
       currentElem = elem;
     } else {
-      currentElem = elem.closest('div.product-item');
+      currentElem = elem.closest('div.product-item-wrapper');
     }
+    var quickElem = currentElem.querySelector('.product-quick-view');
+    var itemElem = currentElem.querySelector('.product-item');
 
-    latestHoverProduct = currentElem;
+    var productId = itemElem.getAttribute('data-product-id');
+    
+    if (!quickElem.classList.contains('loaded') && !quickElem.classList.contains('loading')) {
+      
+      quickElem.classList.add('loading');
 
-    var newNode = document.createElement('div');
-    newNode.innerHTML= loadingQuickView;
+      var promises = [attributeService.getProductAttributes(productId), productService.getProduct(productId)];
+      
+      Promise.all(promises).then(function(data) {
+    	var attributes = data[0];
+    	var product = data[1];
+    	var mappedAttributes = attributeService.mapProductAttributes(attributes);
 
-    currentElem.parentNode.insertBefore(newNode.firstChild, currentElem);
+	var ctx = {attributes: mappedAttributes, product: product};
 
-    var productId = currentElem.getAttribute('data-product-id');
+    	render('_quick_product_view.html', ctx, function(err, res) {
+	  quickElem.innerHTML = res;
+	  quickElem.classList.remove('loading');
+	  quickElem.classList.add('loaded');
 
-    Promise.all(
-      [attributeService.getProductAttributes(productId), productService.getProduct(productId)])
-      .then(function(data) {
-	var attributes = data[0];
-	var product = data[1];
-	var mappedAttributes = attributeService.mapProductAttributes(attributes);
+    	  var button = document.querySelector(
+    	    'div.product-quick-view > div[data-product-id="' + product.product_id + '"] button.add-to-cart');
 
-	render('_quick_product_view.html', {
-	  attributes: mappedAttributes, product: product}, function(err, res) {
-	    var quickAddElement = currentElem.parentNode.querySelector('div.quick-add-to-cart');
-
-	    if (!quickAddElement || latestHoverProduct != currentElem) {
-	      if (quickAddElement) {
-		quickAddElement.remove();
-	      }
-	      return false;
-	    }
-	    quickAddElement.innerHTML = res;
-
-	    var button = document.querySelector(
-	      'div.quick-add-to-cart > div[data-product-id="' + product.product_id + '"] button.add-to-cart');
-
-	    if (button) {
-	      handleAddToCartEvent(button, {
-		productId: product.product_id,
-		productAttributes: mappedAttributes,
-		form: button.closest('form'),
-		afterCallback: renderShoppingcartSummary
-	      });
-	    }
-
-	    var elems = document.querySelectorAll('select');
-	    var instances = M.FormSelect.init(elems, {});
-
-	    let elementsArray = document.querySelectorAll("div.quick-add-to-cart");
-
-	    elementsArray.forEach(function(elem) {
-	      elem.removeEventListener('mouseleave', handleAddToCarMouseLeave);
-	      elem.addEventListener('mouseleave', handleAddToCarMouseLeave);
-	    });
-
-	    return true;
-	  });
-
-      });
-  } else {
-    if (!elem.classList.contains('quick-add-to-cart') && !elem.closest('div.quick-add-to-cart')) {
-      latestHoverProduct = null;  // Mouse outside product or quick add
-      let elementsArray = document.querySelectorAll("div.quick-add-to-cart");
-
-      elementsArray.forEach(function(elem) {
-	elem.remove();
+    	  if (button) {
+    	    handleAddToCartEvent(button, {
+    	      productId: product.product_id,
+    	      productAttributes: mappedAttributes,
+    	      form: button.closest('form'),
+    	      afterCallback: renderShoppingcartSummary
+    	    });
+    	  }
+	});
       });
     }
   }
@@ -193,6 +153,29 @@ function handleCategoryChange(e) {
   }
 }
 
+function handleItemsPerPageChange(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  var elem = e.target;
+  var itemsPerPage = parseInt(elem.getAttribute('data-items'));
+
+  var controller = elem.customParams.controller;
+
+  if (ITEMS_PER_PAGE_LIST.includes(itemsPerPage)) {
+    localStorage.setItem('itemsPerPage', itemsPerPage);
+    controller.itemsPerPage = itemsPerPage;
+
+    var currentItemsElem = document.querySelector('div.items-per-page span.current');
+    if (currentItemsElem) {
+      currentItemsElem.classList.remove('current');
+    }
+    controller.renderProducts();
+    //
+    elem.parentNode.classList.add('current');
+  }
+}
+
 function handleSearchProducts() {
   var searchString = document.querySelector('input[name=search]').value;
   var searchAll = document.querySelector('input[name=search_all]').checked ? 'yes' : 'no';
@@ -216,6 +199,11 @@ export default class HomeController {
     this.categoryId = localStorage.getItem('categoryId') || null;
     this.searchString = getParameterByName('search') || null;
     this.searchAll = getParameterByName('searchAll') || null;
+    this.itemsPerPage = parseInt(localStorage.getItem('itemsPerPage'));
+    if (!ITEMS_PER_PAGE_LIST.includes(this.itemsPerPage)) {
+      this.itemsPerPage = ITEMS_PER_PAGE_LIST[0];
+    }
+
     if (this.searchString) {
       this.categoryId = "";
       localStorage.setItem('categoryId', "");
@@ -229,7 +217,7 @@ export default class HomeController {
 
     return {
       page: this.pageNum,
-      total: Math.ceil(totalRows / ITEMS_PER_PAGE)
+      total: Math.ceil(totalRows / this.itemsPerPage)
     };
   }
 
@@ -247,6 +235,18 @@ export default class HomeController {
     elements.forEach(function(element) {
       element.removeEventListener('click', handleDepartmentChange);
       element.addEventListener('click', handleDepartmentChange);
+      element.customParams = {controller: that};
+    });
+  }
+
+  handleItemsPerPageEvent() {
+    var that = this;
+
+    var elements = document.querySelectorAll('div.items-per-page span a');
+
+    elements.forEach(function(element) {
+      element.removeEventListener('click', handleItemsPerPageChange);
+      element.addEventListener('click', handleItemsPerPageChange);
       element.customParams = {controller: that};
     });
   }
@@ -278,13 +278,13 @@ export default class HomeController {
     var productList;
 
     if (this.searchString) {
-      productList = productService.search(this.searchString, this.searchAll, this.pageNum, ITEMS_PER_PAGE);
+      productList = productService.search(this.searchString, this.searchAll, this.pageNum, this.itemsPerPage);
     } else if (this.categoryId) {
-      productList = productService.getProductsByCategory(this.categoryId, this.pageNum, ITEMS_PER_PAGE);
+      productList = productService.getProductsByCategory(this.categoryId, this.pageNum, this.itemsPerPage);
     } else if (this.departmentId) {
-      productList = productService.getProductsByDepartment(this.departmentId, this.pageNum, ITEMS_PER_PAGE);
+      productList = productService.getProductsByDepartment(this.departmentId, this.pageNum, this.itemsPerPage);
     } else {
-      productList = productService.list(this.pageNum, ITEMS_PER_PAGE);
+      productList = productService.list(this.pageNum, this.itemsPerPage);
     }
 
     productList.then(function(products) {
@@ -299,7 +299,15 @@ export default class HomeController {
       render('_products.html', context, function(err, res) {
 	var productsElem = document.querySelector('div.products-list');
 
-	productsElem.innerHTML= res;
+	// Add pagination
+	render('_pagination.html', context, function(err, pagingRes) {
+	  var pagingElem = document.querySelector('div.paging > div');
+	  pagingElem.innerHTML = pagingRes;
+
+	  productsElem.innerHTML= res;
+	});
+
+
       });
     });
   }
@@ -313,7 +321,10 @@ export default class HomeController {
  	departmentId: that.departmentId,
 	categoryId: that.categoryId,
 	searchString: that.searchString,
+	itemsList: ITEMS_PER_PAGE_LIST,
+	itemsPerPage: that.itemsPerPage,
 	searchAll: that.searchAll
+
       };
 
       render('home.html', context, function(err, res) {
@@ -327,6 +338,7 @@ export default class HomeController {
 	M.updateTextFields();
 	that.handleQuickAddToCartEvent();
 	that.handleDepartmentChangeEvent();
+	that.handleItemsPerPageEvent();
 	handleSearchEvent();
 	if (that.departmentId) {
 	  that.renderCategories();
